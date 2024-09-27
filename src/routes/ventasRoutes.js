be59cba3ab73b0db5acbaf8df66db80e818c78f3
unknown <paolcemo@gmail.com>
2024-09-27 08:26:18 -0500

    Descripción de los cambios

--- src/routes/ventasRoutes.js
+++ src/routes/ventasRoutes.js
@@ -8,16 +8,15 @@ const {
 
 const router = express.Router();
 
-// Ruta para crear una nueva venta
 router.post('/', crearVenta);
 
-// Ruta para obtener todas las ventas
+
 router.get('/', obtenerVentas);
 
-// Ruta para actualizar una venta por ID
+
 router.put('/:id', actualizarVenta);
 
-// Ruta para anular una venta por ID
-router.patch('/:id/anular', anularVenta); // Usamos PATCH para anular
+
+router.patch('/:id/anular', anularVenta);
 
 module.exports = router;
